e7296a53d5ebc599bdb04e630b7118599e182658
Hiroto-Aizawa <r_20love@icloud.com>
2025-10-14 00:03:51 +0900

    update page_1.py

--- web_app/pages/page_1.py
+++ web_app/pages/page_1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown('# データ分析アプリ')
 
@@ -12,12 +13,18 @@ st.code(code, language='python')
 
 uploaded_file = st.file_uploader('CSVファイルをアップロードしてください', type=['csv'])
 if uploaded_file is not None:
-    import pandas as pd
+    st.info('ファイルがアップロードされました。')
 
     df = pd.read_csv(uploaded_file)
     st.write('データのプレビュー:')
-    st.dataframe(df.head())
+    st.dataframe(df.head(5)) # 先頭5行だけ表示
 
-    if st.button('基本統計量を表示'):
-        st.write('基本統計量:')
-        st.write(df.describe())
+    column_names = [c for c in df.columns]
+    target_column = st.selectbox(
+        'ターゲットを選択してください', 
+        column_names
+    )
+    st.info(f'{target_column}を予測対象として予測モデルを作ります！')
+    # if st.button('基本統計量を表示'):
+    #     st.write('基本統計量:')
+    #     st.write(df.describe())
