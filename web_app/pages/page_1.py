3591a4b72a497a880700e8c2ded0efdbb0319a2a
Hiroto-Aizawa <r_20love@icloud.com>
2025-10-13 23:55:28 +0900

    add fileuploader

--- web_app/pages/page_1.py
+++ web_app/pages/page_1.py
@@ -1,9 +1,23 @@
 import streamlit as st
 
+st.markdown('# データ分析アプリ')
+
 code = '''
 import streamlit as st
 
-st.title('サンプルアプリ')
+st.title('ファイルをアップロードしてデータ分析')
 '''
 
 st.code(code, language='python')
+
+uploaded_file = st.file_uploader('CSVファイルをアップロードしてください', type=['csv'])
+if uploaded_file is not None:
+    import pandas as pd
+
+    df = pd.read_csv(uploaded_file)
+    st.write('データのプレビュー:')
+    st.dataframe(df.head())
+
+    if st.button('基本統計量を表示'):
+        st.write('基本統計量:')
+        st.write(df.describe())
